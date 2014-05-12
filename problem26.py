1ca5060ed325462f6587f73272beae86e8ed1a7d
drb1001 <pi_anyone@hotmail.com>
2014-05-13 02:29:00 +0900

    adding all old problems

--- problem26.py
+++ problem26.py
@@ -1,3 +1,28 @@
+# Euler challenge
+# Problem 26
+
+def shortendec(n):
+    output="0."
+    return output
+
+
+output=[]
+for i in range(900,1000):
+    d=1.0/i
+    strd=str(d)
+    print strd
+    
+    if len(strd)>11:
+        if not (strd[9]==strd[10] and strd[9]==strd[11] and strd[9]==strd[12]):
+            output.append(d)
+
+
+print len(output)
+print output
+    
+
+
+
 
 def reccurcyc(d):
 
--- problem32.py
+++ problem32.py
@@ -1,3 +1,26 @@
+# project euler
+# problem 32
+
+
+for a in range(1,100):
+    print a
+    for b in range(1000000,999999999):
+        m=a*b
+
+        s=str(a)+str(b)+str(m)
+        sarray=[]
+        for letter in s:
+            sarray.append(letter)
+        sarray.sort()
+        if s=="123456789":
+            print a,b,m,s
+
+
+
+
+
+
+
 import itertools
 
 output = []
