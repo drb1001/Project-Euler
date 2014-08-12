22feb9e467168a308f874defd545447dc8d956e4
drb1001 <pi_anyone@hotmail.com>
2014-08-13 01:26:33 +0200

    Retrieving and inserting problem description from html and testing script on 2 problems

--- web files/getproblem.py
+++ web files/getproblem.py
@@ -1,22 +1,54 @@
 import urllib
-import bs4
+from bs4 import BeautifulSoup
+import os
 
-var = "2" #raw_input("Problem number: ")
-print var
+
+# specify problem number
+var = raw_input("Problem number: ")
 myurl = "https://projecteuler.net/problem=" + var
 
 
-
+# retrieve html
+print myurl
+print "Retrieving html ... "
 sock = urllib.urlopen(myurl)
 htmlSource = sock.read()
 sock.close()
 
-print (htmlSource)
+
+# extract and format text
+soup = BeautifulSoup(htmlSource)
+
+mytitle = soup.html.head.title.get_text()
+
+mycontent = soup.find("div", {"class": "problem_content"})
+mycontent = mycontent.findAll(text=True)
+mycontent = [x.replace('\n', '\n#  ') for x in mycontent]
+mycontent = "".join(mycontent)
+
+myoutput = "\n###\n#  " + mytitle \
+           + "\n#  " + myurl \
+           + "\n#  " \
+           + "\n#  " + mycontent \
+           + "\n###\n\n"
+
+print myoutput
 
 
-file = open('myfile.dat', 'w+')
-
-# extract data
 # check if file exists
+filename = "problem" + var + ".py"
+myrootdir = '..'
+
+for root, dirs, files in os.walk(myrootdir):
+    for allfiles in files:
+        if allfiles == filename:
+            myfilepath = root + "/" + allfiles
+
+
 # if file exists add as comment
+print myfilepath
+with file(myfilepath, 'r') as original:
+    data = original.read()
+    with file(myfilepath, 'w') as modified: modified.write(myoutput + data)
+
 # if file does not exist - create file and add comment
