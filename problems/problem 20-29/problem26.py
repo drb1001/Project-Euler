317a875e014ac3932fdb7ac49ac962da2395ac5e
Daniel Brown <dbrown@podio.com>
2016-02-18 18:06:47 +0100

    fix problem 26

--- problems/problem 20-29/problem26.py
+++ problems/problem 20-29/problem26.py
@@ -1,26 +1,3 @@
-# Project Euler
-# Problem 26
-
-def shortendec(n):
-    output="0."
-    return output
-
-
-output=[]
-for i in range(900,1000):
-    d=1.0/i
-    strd=str(d)
-    print strd
-
-    if len(strd)>11:
-        if not (strd[9]==strd[10] and strd[9]==strd[11] and strd[9]==strd[12]):
-            output.append(d)
-
-
-print len(output)
-print output
-
-
 
 ###
 #  Problem 26 - Project Euler
@@ -44,33 +21,63 @@ print output
 #
 ###
 
-
-def reccurcyc(d):
-
-     #for loop - try 9, else try 9*10 + 9 else *10 +9 ...
-    mult = 9.0
-    l = 1
-    while True:
-
-        if mult/d % 1 == 0 : break
-        else :
-            mult = mult *10 + 9
-            l = l+1
-
-    return [d, 1.0/d, l]
+# Project Euler
+# Problem 26
 
 
-#def longdiv(n,m):
+# d = 2. 1/2 -> 10/2 = 5 r0. (not repeating)
+# d = 3. 1/3 -> 10/3 = 3 r1. 1/3 -> 10/3 = 3 r1. (repeating, length 1)
+# d = 6. 1/6 -> 10/6 = 1 r4. 4/6 -> 40/6 = 6 r4. (repeating, length 1).
+# d = 7. 10/7 = 1 r 3. 30/7 = 4 r2. 20/7 = 2 r6. 60/7 = 8 r4. 40/7 = 5 r5. 50/7 = 7 r1. 10/7 is a repeat.
+# d = 9. 1/9 -> 10/9 = 1 r1. 1/9-> 10/9 = 1 r1. (repeating, length 1).
+# d = 11. 1/11 -> 100/11 = 9 r1. 1/11 -> 100/11 = 9 r1. (repeating, length 1).
+# d = 12. 1/12 -> 100/12 = 8 r4. 4/12 -> 40/12 = 3 r4. 4/12 -> 40/12 = 3 r4. (repeating, length 1)
+# d = 13. 1/13 -> 100/13 = 7 r9. 9/13-> 90/13 = 6 r12. 12/13 -> 100/13 = ...
 
 
-
-m = [0,0]
+max_rep = 0
+max_n = 0
+max_s = ""
 
 for d in range(2,1000):
-    if (d/2.0 == d/2 or d/5.0 == d/5): continue
-    print reccurcyc(d)
-    # mnew = reccurcyc(d)
-    #if m[1] > mnew[1]:
-    #    m = mnew
+    output = []
+    s = "0."
+    rep_len = 0
+    r = 1
+    while True:
+        while r < d: r = r*10
+        len_r = len(str(r))
+        q = r//d
+        r = r%d
 
-print m
+        if (r==0):
+            output.append([q,r,len_r])
+            rep_len = 0
+            for x in output:
+                s = s + str(x[0]).zfill(x[2]-1)
+            break
+
+        if ([q,r,len_r]) in output:
+            p = output.index([q,r,len_r])
+            #print output
+            for x in output[0:p]:
+                s = s + str(x[0]).zfill(x[2]-1)
+            s = s + "("
+            for x in output[p:]:
+                s = s + str(x[0])
+            s = s + ")"
+            rep_len = len(output[p:])
+            break
+
+        output.append([q,r,len_r])
+
+    print "d=%s. 1/d = %s \t (= %s) \t(rep. length = %s)" % (d, s, 1.0/d, rep_len)
+
+    if rep_len > max_rep:
+        max_rep = rep_len
+        max_n = d
+        max_s = s
+        print "  *********"
+
+print " -------"
+print max_n, max_rep, max_s
